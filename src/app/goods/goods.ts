cf01acc2dd0c4860ed988cfd17ad1dcb048e3b88
BijonTalukder <bijontalukder1247@gmail.com>
2025-08-26 14:37:39 +0600

    updated

--- src/app/goods/goods.ts
+++ src/app/goods/goods.ts
@@ -9,10 +9,45 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './goods.css'
 })
 export class Goods {
-   goodsName = '';
-onSubmit() {
-  //  event.preventDefault();
-  let name = this.goodsName ;
-  console.log('Goods calling!',name);
+   goodsList: any[] = [];
+
+  colors = ['Red', 'Blue', 'Green'];
+  widths = ['Small', 'Medium', 'Large'];
+  packages = ['Box', 'Bag', 'Packet'];
+  units = ['Piece', 'Kg', 'Meter'];
+
+  articleNo!: string;
+  color!: string;
+  width!: string;
+  package!: string;
+  unit!: string;
+  weight!: number;
+  gsm!: number;
+  isAvailable: boolean = false;
+
+  onSubmit() {
+    const newGoods = {
+      articleNo: this.articleNo,
+      color: this.color,
+      width: this.width,
+      package: this.package,
+      unit: this.unit,
+      weight: this.weight,
+      gsm: this.gsm,
+      isAvailable: this.isAvailable,
+    };
+
+    this.goodsList.push(newGoods);
+    // Reset form
+    this.articleNo = '';
+    this.color = '';
+    this.width = '';
+    this.package = '';
+    this.unit = '';
+    this.weight = 0;
+    this.gsm = 0;
+    this.isAvailable = false;
+
+  
   }
 }
